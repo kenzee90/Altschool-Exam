3edc191c146337b5a5f15c21c62e1de95aaecd43
kenzee90 <pamkenneth78@gmail.com>
2024-04-20 08:16:35 +0100

    react-router-dom source connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AllRepositoryList from './AllRepositoryList';
 import SingleRepositoryPage from './SingleRespositoryPage';
